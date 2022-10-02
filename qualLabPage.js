ab3f8d1966a37f436e413d30a6576e14ceea7d0e
Samiksha Shrivastava <ssamiksha510@gmail.com>
2022-10-02 15:19:25 +0530

    Sending initial file as a backup.

---
+++ qualLabPage.js
@@ -0,0 +1,350 @@
+function addCode(click){
+    var choice= click;
+
+    switch(choice){
+        case '1': 
+            document.getElementById("details").innerHTML += 
+            "<style>" +
+            "table {" +
+                "border-collapse: collapse;" +
+                "width: 100%;" +
+             " }" +
+              
+              "th, td { " +
+                "text-align: left;" +
+                "padding: 8px;" +
+             " }" +
+              
+              "tr:nth-child(even) {background-color: #c1e4f8;}" +
+            "</style>" +
+
+                "<table class=\"Computer-1\">" +
+                    "<h2>COMP0001 Information<hr></h2>" +
+                    "<tr>" +
+                        "<th>Device Name</th>" +
+                        "<th>Processor</th>" +
+                        "<th>Storage</th>" +
+                        "<th>Installed RAM</th>" +
+                        "<th>Operating System</th>" +
+                    "</tr>" +
+                    "<tr>" +   
+                        "<td>Lenovo Legion-1</td>" +
+                        "<td>Core i7</td>" +
+                        "<td>512 GB SSD</td>" +
+                        "<td>16 GB</td>" +
+                        "<td>Windows 11</td>" +
+                    "</tr>" +
+                "</table>";
+                break; 
+
+        case '2': 
+            document.getElementById("details").innerHTML += 
+            "<style>" +
+            "table {" +
+                "border-collapse: collapse;" +
+                "width: 100%;" +
+             " }" +
+              
+              "th, td { " +
+                "text-align: left;" +
+                "padding: 8px;" +
+             " }" +
+              
+              "tr:nth-child(even) {background-color: #c1e4f8;}" +
+            "</style>" + 
+            
+                "<table class=\"Computer-2\">" +
+                    "<h2>COMP0010 Information<hr></h2>" +
+                    "<tr>" +
+                        "<th>Device Name</th>" +
+                        "<th>Processor</th>" +
+                        "<th>Storage</th>" +
+                        "<th>Installed RAM</th>" +
+                        "<th>Operating System</th>" +
+                    "</tr>" +
+                    "<tr>" +   
+                        "<td>Lenovo Legion-2</td>" +
+                        "<td>Core i7</td>" +
+                        "<td>512 GB SSD</td>" +
+                        "<td>16 GB</td>" +
+                        "<td>Windows 11</td>" +
+                    "</tr>" +
+                "</table>" ; 
+                break;      
+
+            case '3': 
+            document.getElementById("details").innerHTML += 
+            "<style>" +
+            "table {" +
+                "border-collapse: collapse;" +
+                "width: 100%;" +
+             " }" +
+              
+              "th, td { " +
+                "text-align: left;" +
+                "padding: 8px;" +
+             " }" +
+              
+              "tr:nth-child(even) {background-color: #c1e4f8;}" +
+            "</style>" + 
+                "<table class=\"Computer-3\">" +
+                    "<h2>COMP0011 Information<hr></h2>" +
+                    "<tr>" +
+                        "<th>Device Name</th>" +
+                        "<th>Processor</th>" +
+                        "<th>Storage</th>" +
+                        "<th>Installed RAM</th>" +
+                        "<th>Operating System</th>" +
+                    "</tr>" +
+                    "<tr>" +   
+                        "<td>Lenovo Legion-3</td>" +
+                        "<td>Core i7</td>" +
+                        "<td>512 GB SSD</td>" +
+                        "<td>16 GB</td>" +
+                        "<td>Windows 11</td>" +
+                    "</tr>" +
+                "</table>" ; 
+                break;    
+
+            case '4': 
+            document.getElementById("details").innerHTML += 
+            "<style>" +
+            "table {" +
+                "border-collapse: collapse;" +
+                "width: 100%;" +
+             " }" +
+              
+              "th, td { " +
+                "text-align: left;" +
+                "padding: 8px;" +
+             " }" +
+              
+              "tr:nth-child(even) {background-color: #c1e4f8;}" +
+            "</style>" + 
+                "<table class=\"Computer-4\">" +
+                    "<h2>COMP0100 Information<hr></h2>" +
+                    "<tr>" +
+                        "<th>Device Name</th>" +
+                        "<th>Processor</th>" +
+                        "<th>Storage</th>" +
+                        "<th>Installed RAM</th>" +
+                        "<th>Operating System</th>" +
+                    "</tr>" +
+                    "<tr>" +   
+                        "<td>Lenovo Legion-4</td>" +
+                        "<td>Core i7</td>" +
+                        "<td>512 GB SSD</td>" +
+                        "<td>16 GB</td>" +
+                        "<td>Windows 11</td>" +
+                    "</tr>" +
+                "</table>" ; 
+                break;    
+
+            case '5': 
+            document.getElementById("details").innerHTML += 
+            "<style>" +
+            "table {" +
+                "border-collapse: collapse;" +
+                "width: 100%;" +
+             " }" +
+              
+              "th, td { " +
+                "text-align: left;" +
+                "padding: 8px;" +
+             " }" +
+              
+              "tr:nth-child(even) {background-color: #c1e4f8;}" +
+            "</style>" + 
+                "<table class=\"Computer-2\">" +
+                    "<h2>COMP0101 Information<hr></h2>" +
+                    "<tr>" +
+                        "<th>Device Name</th>" +
+                        "<th>Processor</th>" +
+                        "<th>Storage</th>" +
+                        "<th>Installed RAM</th>" +
+                        "<th>Operating System</th>" +
+                    "</tr>" +
+                    "<tr>" +   
+                        "<td>Lenovo Legion-5/td>" +
+                        "<td>Core i7</td>" +
+                        "<td>512 GB SSD</td>" +
+                        "<td>16 GB</td>" +
+                        "<td>Windows 11</td>" +
+                    "</tr>" +
+                "</table>" ; 
+                break;    
+
+            case '6': 
+            document.getElementById("details").innerHTML += 
+            "<style>" +
+            "table {" +
+                "border-collapse: collapse;" +
+                "width: 100%;" +
+             " }" +
+              
+              "th, td { " +
+                "text-align: left;" +
+                "padding: 8px;" +
+             " }" +
+              
+              "tr:nth-child(even) {background-color: #c1e4f8;}" +
+            "</style>" + 
+                "<table class=\"Computer-6\">" +
+                    "<h2>COMP0110 Information<hr></h2>" +
+                    "<tr>" +
+                        "<th>Device Name</th>" +
+                        "<th>Processor</th>" +
+                        "<th>Storage</th>" +
+                        "<th>Installed RAM</th>" +
+                        "<th>Operating System</th>" +
+                    "</tr>" +
+                    "<tr>" +   
+                        "<td>Lenovo Legion-6</td>" +
+                        "<td>Core i7</td>" +
+                        "<td>512 GB SSD</td>" +
+                        "<td>16 GB</td>" +
+                        "<td>Windows 11</td>" +
+                    "</tr>" +
+                "</table>" ; 
+                break;    
+
+            case '7': 
+            document.getElementById("details").innerHTML += 
+            "<style>" +
+            "table {" +
+                "border-collapse: collapse;" +
+                "width: 100%;" +
+             " }" +
+              
+              "th, td { " +
+                "text-align: left;" +
+                "padding: 8px;" +
+             " }" +
+              
+              "tr:nth-child(even) {background-color: #c1e4f8;}" +
+            "</style>" + 
+                "<table class=\"Computer-7\">" +
+                    "<h2>COMP0111 Information<hr></h2>" +
+                    "<tr>" +
+                        "<th>Device Name</th>" +
+                        "<th>Processor</th>" +
+                        "<th>Storage</th>" +
+                        "<th>Installed RAM</th>" +
+                        "<th>Operating System</th>" +
+                    "</tr>" +
+                    "<tr>" +   
+                        "<td>Lenovo Legion-7</td>" +
+                        "<td>Core i7</td>" +
+                        "<td>512 GB SSD</td>" +
+                        "<td>16 GB</td>" +
+                        "<td>Windows 11</td>" +
+                    "</tr>" +
+                "</table>" ; 
+                break;    
+
+            case '8': 
+            document.getElementById("details").innerHTML += 
+            "<style>" +
+            "table {" +
+                "border-collapse: collapse;" +
+                "width: 100%;" +
+             " }" +
+              
+              "th, td { " +
+                "text-align: left;" +
+                "padding: 8px;" +
+             " }" +
+              
+              "tr:nth-child(even) {background-color: #c1e4f8;}" +
+            "</style>" + 
+                "<table class=\"Computer-8\">" +
+                    "<h2>COMP1000 Information<hr></h2>" +
+                    "<tr>" +
+                        "<th>Device Name</th>" +
+                        "<th>Processor</th>" +
+                        "<th>Storage</th>" +
+                        "<th>Installed RAM</th>" +
+                        "<th>Operating System</th>" +
+                    "</tr>" +
+                    "<tr>" +   
+                        "<td>Lenovo Legion-8</td>" +
+                        "<td>Core i7</td>" +
+                        "<td>512 GB SSD</td>" +
+                        "<td>16 GB</td>" +
+                        "<td>Windows 11</td>" +
+                    "</tr>" +
+                "</table>" ; 
+                break;  
+                
+            case '9': 
+            document.getElementById("details").innerHTML += 
+            "<style>" +
+            "table {" +
+                "border-collapse: collapse;" +
+                "width: 100%;" +
+             " }" +
+              
+              "th, td { " +
+                "text-align: left;" +
+                "padding: 8px;" +
+             " }" +
+              
+              "tr:nth-child(even) {background-color: #c1e4f8;}" +
+            "</style>" + 
+                "<table class=\"Computer-9\">" +
+                    "<h2>COMP1001 Information<hr></h2>" +
+                    "<tr>" +
+                        "<th>Device Name</th>" +
+                        "<th>Processor</th>" +
+                        "<th>Storage</th>" +
+                        "<th>Installed RAM</th>" +
+                        "<th>Operating System</th>" +
+                    "</tr>" +
+                    "<tr>" +   
+                        "<td>Lenovo Legion-9</td>" +
+                        "<td>Core i7</td>" +
+                        "<td>512 GB SSD</td>" +
+                        "<td>16 GB</td>" +
+                        "<td>Windows 11</td>" +
+                    "</tr>" +
+                "</table>" ; 
+                break;    
+
+                case '10': 
+                document.getElementById("details").innerHTML += 
+                "<style>" +
+                "table {" +
+                    "border-collapse: collapse;" +
+                    "width: 100%;" +
+                 " }" +
+                  
+                  "th, td { " +
+                    "text-align: left;" +
+                    "padding: 8px;" +
+                 " }" +
+                  
+                  "tr:nth-child(even) {background-color: #c1e4f8;}" +
+                "</style>" + 
+                    "<table class=\"Computer-10\">" +
+                        "<h2>COMP1010 Information<hr></h2>" +
+                        "<tr>" +
+                            "<th>Device Name</th>" +
+                            "<th>Processor</th>" +
+                            "<th>Storage</th>" +
+                            "<th>Installed RAM</th>" +
+                            "<th>Operating System</th>" +
+                        "</tr>" +
+                        "<tr>" +   
+                            "<td>Lenovo Legion-10</td>" +
+                            "<td>Core i7</td>" +
+                            "<td>512 GB SSD</td>" +
+                            "<td>16 GB</td>" +
+                            "<td>Windows 11</td>" +
+                        "</tr>" +
+                    "</table>" ; 
+                    break;    
+
+
+
+    }
+}
